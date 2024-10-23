98bac015f60dcd48744e3fa7ae75f07f5fea3754
laxmanparasuraman <laxmanparasuramanr@gmail.com>
2024-10-24 01:22:04 +0530

    learn react

---
+++ vs code/Ineuron/Namaste react/netflix-gpt/notes.js
@@ -0,0 +1,23 @@
+// 20 min
+
+
+/* 
+
+FEATURES
+Login/SignUp
+   signin sign up 
+   redirect to Browserpage
+
+ Browser(Afer Authenticatiom)
+   -Header 
+   -Main
+     -trailer in bcground
+     -Title and description
+     -movie List
+        -vertical Scrollable
+
+NetflixGpt
+    -Search BAr
+    -Movie List
+
+*/
